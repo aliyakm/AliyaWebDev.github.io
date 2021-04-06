9cc9f4cc04ebcff3572c0b86ad6092869cdb526e
aliyakm <57257098+aliyakm@users.noreply.github.com>
2021-04-06 20:09:03 +0600

    1

---
+++ lab8/demo/main/views.py
@@ -0,0 +1,30 @@
+from django.http.response import HttpResponse, JsonResponse
+
+def hello(request):
+    return HttpResponse('hello message')
+
+def hours_heads(request, hours):
+    return HttpResponse(f'<h1>Hours Heads: {hours}</h1>')
+
+# def products_list(request):
+#     return HttpResponse('<h1>List of products</h1>')
+#
+# def product_detail(request, product_id):
+#     return HttpResponse(f'<h1>Product page, id: {product_id}</h1>')
+
+products = [
+    {
+        'id': i,
+        'name': f'Product {i}',
+        'price': i*1000
+    } for i in range(1, 11)
+]
+
+def products_list(request):
+    return JsonResponse(products, safe=False)
+
+def product_detail(request, product_id):
+    for product in products:
+        if product['id'] == product_id:
+            return JsonResponse(product)
+    return JsonResponse({'message': 'Product not found'})
